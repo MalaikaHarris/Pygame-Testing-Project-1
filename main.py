236e77c670fdec9a68a70397ab00165da41e66e0
Malaika Harris <malaika.harris@ebrooke.org>
2020-12-11 22:01:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame, sys, time
+
+#Initialize pygame so that we can use all of the built in functions in the pygame library
+
+pygame.init()
+
+windowSize = (800, 600)
+
+screen = pygame.display.set_mode(windowSize)
+
+myriadProFont = pygame.font.SysFont('myriadProFont', 48)
+
+helloWorldText = myriadProFont.render('hello world', 1,(100,100,255), (255,255,255))
+
+x,y = 0,0
+
+while 1:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT: sys.exit()
